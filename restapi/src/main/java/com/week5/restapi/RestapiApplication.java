3d531bfcc663e261b4f9044352b23123c5ae7a7d
Aanand <aanandsaini968@gmail.com>
2026-02-12 12:17:28 -0500

    added Link for the project

--- restapi/src/main/java/com/week5/restapi/RestapiApplication.java
+++ restapi/src/main/java/com/week5/restapi/RestapiApplication.java
@@ -2,7 +2,6 @@ package com.week5.restapi; // or com.weekNo.restapi
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.restclient.RestTemplateBuilder;
@@ -16,10 +15,15 @@ public class RestapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestapiApplication.class, args);
+        System.out.println("Application Started Successfully -> Check the link below");
+        System.out.println("http://localhost:8080/countries/US");
     }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
+
+
+
 }
